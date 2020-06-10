da35f3ac2da53ea645f6b18471c84b57937a72fa
mazipazi <66731402+mazipazi@users.noreply.github.com>
2020-06-10 14:21:03 +0200

    Piaty commit

--- 01program.php
+++ 01program.php
@@ -3,5 +3,6 @@ echo ("Witaj");
 echo("<br> jestem programem 01");
 echo("<br> Tak wlasnie jestem programem");
 echo ("<br>Dalsza czesc programu");
+echo ("<br>Dalsza czesc programu z Githuba");
 ?>
 
